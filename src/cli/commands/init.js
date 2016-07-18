eb2a725950b750cb08ad4b7f7a1d00e10f7ad8d9
Nathan Ginnever <ginneversource@gmail.com>
2016-07-18 14:49:08 -0700

    Update init.js

--- src/cli/commands/init.js
+++ src/cli/commands/init.js
@@ -4,18 +4,18 @@ const Command = require('ronin').Command
 const utils = require('../utils')
 
 module.exports = Command.extend({
-	desc: 'initialize package.json',
+  desc: 'initialize package.json',
 
-	options: {
-		force: {
-			alias: 'f',
-			type: 'boolean'
-		}
-	},
+  options: {
+    force: {
+      alias: 'f',
+      type: 'boolean'
+    }
+  },
 
-	run: (force) => {
-		utils.getIPPM((err, ippm) => {
-			ippm.init()
-		})
-	}
+  run: (force) => {
+    utils.getIPPM((err, ippm) => {
+      ippm.init()
+    })
+  }
 })
