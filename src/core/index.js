6fc8bcea0163c6da246cb357ae694cf58a54dbf4
Nathan Ginnever <ginneversource@gmail.com>
2016-07-18 14:56:34 -0700

    Update index.js

--- src/core/index.js
+++ src/core/index.js
@@ -6,10 +6,10 @@ const init = require('./IPPM/init')
 exports = module.exports = IPPM
 
 function IPPM () {
-	if(!(this instanceof IPPM)) {
-		throw new Error('Must be instantiated with new')
-	}
-
-	this.test = test(this)
-	this.init = init(this)
+  if(!(this instanceof IPPM)) {
+    throw new Error('Must be instantiated with new')
+  }
+  
+  this.test = test(this)
+  this.init = init(this)
 }
