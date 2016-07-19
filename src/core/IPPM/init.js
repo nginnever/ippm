b16236f1137c5b44c63bc738b4a2394cda97423b
nginnever <ginneversource@gmail.com>
2016-07-18 23:01:15 -0700

    init command

--- src/core/IPPM/init.js
+++ src/core/IPPM/init.js
@@ -1,12 +1,80 @@
 'use strict'
 
+const jsonfile = require('jsonfile')
+const prompt = require('prompt')
+const colors = require("colors/safe")
+
 module.exports = function init (self) {
   return(opts, callback) => {
     if (typeof opts === 'function') {
       callback = opts
       opts = {}
     }
+    console.log('Init will walk you through creating a package.json file now!')
+    
+    const properties = [
+    	{
+    		name: 'name'
+    	},
+    	{
+    		name: 'version'
+    	},
+    	{
+    		name: 'description'
+    	},
+    	{
+    		name: 'entry'
+    	},
+    	{
+    		name: 'test'
+    	},
+    	{
+    		name: 'github'
+    	},
+    	{
+    		name: 'keywords'
+    	},
+    	{
+    		name: 'author'
+    	},
+    	{
+    		name: 'license'
+    	},
+    	{
+    		name: 'description'
+    	}
+    ]
 
-    console.log('init command core')
+    const file = process.cwd() + '/package.json'
+    var pkg = {}
+
+    prompt.message = ''
+    prompt.start()
+    prompt.get(properties, (err, res) => {
+    	if (err) { return onErr(err) }
+      
+    	pkg["name"] = res.name
+    	pkg["version"] = res.version
+    	pkg["description"] = res.description
+    	pkg["main"] = res.entry
+    	pkg["scripts"] = {}
+    	pkg.scripts["test"] = res.test
+    	pkg["author"] = res.author
+    	pkg["license"] = res.license
+
+    	console.log(JSON.stringify(pkg))
+    	jsonfile.writeFile(file, pkg, {spaces: 2}, (err) => {
+    		if (err) {
+    			console.log(err)
+    		}
+    		console.log('Wrote package.json at: ', process.cwd())
+    	})
+      console.log('Test: ' + res.testing)
+    })
+
+	  function onErr(err) {
+	    console.log(err)
+	    return 1
+	  }
   }
 }
